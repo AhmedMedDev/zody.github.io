9f44988cd99740542299d1f210fb18d166f3718b
dedoa82 <67242566+dedoa82@users.noreply.github.com>
2020-10-12 07:26:47 +0200

    Add files via upload

---
+++ js/slick.js
@@ -0,0 +1,51 @@
+////testimonial WHAT 
+//     $('.whatContent .row ').slick({
+//      prevArrow:$('.whatContent .prev'),
+//      nextArrow:$('.whatContent .next'),
+//      dots: true,
+//      infinite: true,
+//      slidesToShow: 2,
+//      slidesToScroll: 2,
+//      autoplay:true,
+//      autoplaySpeed:1000,
+//      responsive: [
+//            {
+//                breakpoint: 990,
+//                settings: {
+//                slidesToShow: 1,
+//                slidesToScroll: 1,
+//                infinite: true,
+//              }
+//            },
+//          {
+//                breakpoint: 576,
+//                settings: {
+//                slidesToShow: 1,
+//                slidesToScroll: 1,
+//                infinite: true,
+//                dots: false,
+//              }
+//            },
+//          
+//          ]
+//
+//
+//      
+//  });
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
